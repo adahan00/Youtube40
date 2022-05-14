4615b98f1b5d19a6a73340e1d34f2c97293d43d3
adahan00 <adahankasymaliev6@gmail.com>
2022-05-14 19:03:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/youtube40/common/object/Constant.kt
@@ -0,0 +1,8 @@
+package com.example.youtube40.common.`object`
+
+object Constant {
+
+    const val channelId = "UCt7sv-NKh44rHAEb-qCCxvA"
+    const val part = "snippet,contentDetails"
+    const val putId = "key_id"
+}
